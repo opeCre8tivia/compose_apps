95cd0b192b0936c3125c79fa543c415182c13167
opeCre8tivia <opegude.n3t@gmail.com>
2023-06-12 15:20:25 +0300

    first screen test

--- BizCard/app/src/main/java/com/odavolt/bizcard/MainActivity.kt
+++ BizCard/app/src/main/java/com/odavolt/bizcard/MainActivity.kt
@@ -3,12 +3,16 @@ package com.odavolt.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.odavolt.bizcard.ui.theme.BizCardTheme
 
@@ -17,27 +21,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BizCardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+               CreateBizCard()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CreateBizCard (){
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxWidth()
+            .background(color = Color(0xffffffff))
+
+    ) {
+         Card(
+            modifier = Modifier.background(color = Color.White)
+         ){
+            Text(text = "Compose app")
+         }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    BizCardTheme {
-        Greeting("Android")
-    }
+    CreateBizCard()
 }
